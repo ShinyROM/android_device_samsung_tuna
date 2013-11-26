fae714e4cf18d48fcba06e26dba079b578cb03ee
Christian Gaetano <cgatno@gmail.com>
2013-11-25 20:51:35 -0500

    We don't want all of these live wallpapers

--- device.mk
+++ device.mk
@@ -106,9 +106,7 @@ PRODUCT_PACKAGES += \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-        LiveWallpapers \
         LiveWallpapersPicker \
-        VisualizationWallpapers \
         librs_jni
 
 # Key maps
